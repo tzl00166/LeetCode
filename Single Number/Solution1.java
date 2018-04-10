d2ec5ad6212136e9b5bcdda7f14519850ae695d9
tzl00166 <leotangwork@gmail.com>
2018-04-10 17:31:10 -0500

    loops in loops

---
+++ Single Number/Solution1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {     
+        int i = 0;
+        int j = 0;
+        while (i < nums.length){
+            j = 0;
+            while (j < nums.length){
+                if ((nums[i] == nums[j]) && (i != j))
+                    break;
+                j++;
+            }
+            if ((j == nums.length) && (nums[i] != nums[j-1])){
+                return nums[i];
+            }
+            i++;
+        }
+        
+        return nums[i-1];
+    }
+}
