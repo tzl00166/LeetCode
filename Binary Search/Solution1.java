e6484dd7044965e2ce914bf1b6f21988c7df8272
tzl00166 <leotangwork@gmail.com>
2018-05-02 15:35:51 -0500

    Solution check in.

---
+++ Binary Search/Solution1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right){
+            if (nums[(left + right) / 2] == target)
+                return (left + right) / 2;
+            else if (nums[(left + right) / 2] > target)
+                right = (left + right) / 2 - 1;
+            else
+                left = (left + right) / 2 + 1;
+        }
+            
+        return -1;
+    }
+}
