b8a83c6f4783ae12a6c6d80b972074a165c83e4f
tzl00166 <leotangwork@gmail.com>
2018-05-30 13:19:31 -0500

    Quiz with answer check in.

--- Search in a Binary Search Tree/Solution1.java
+++ Search in a Binary Search Tree/Solution1.java
@@ -7,6 +7,16 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+/* 
+Question:
+If you do not know the height of the BST h but you are given the total number of nodes N of the BST, can you express the time complexity and space complexity using N instead of h?
+
+Hint:
+What's the difference of the relationship between N and h in the best case and the relationship in the worst case?
+
+Answer: Best case is O(LogN), worst case is N.
+*/
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         while(root != null){
