e61ac04b28ab103df271477ddd2d62eaf8712951
Leo <leotangwork@gmail.com>
2018-06-21 00:03:50 -0500

    Temp solution check in.

---
+++ 387. First Unique Character in a String/Solution1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.get(c) == null)
+                map.put(c, 1);
+            else{
+                int temp = map.get(c);
+                map.put(c, temp + 1);
+            }         
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.get(c) == 1)
+                return i;
+        }
+        
+        return -1;
+    }
+}
