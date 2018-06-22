661be6b63cac98baf9dbb1f26787c7b92ed91897
Leo <leotangwork@gmail.com>
2018-06-22 00:08:17 -0500

    Solutions check in.

---
+++ 219. Contains Duplicate II/Solution1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int before = map.get(nums[i]);
+                if(i - before <= k)
+                    return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
