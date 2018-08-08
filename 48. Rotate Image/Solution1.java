9f73d069f39ed17e3b87b2f683de44c0235f6713
tzl00166 <leotangwork@gmail.com>
2018-08-08 17:38:15 -0500

    48. Rotate Image check in.

---
+++ 48. Rotate Image/Solution1.java
@@ -0,0 +1,37 @@
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+ *
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        if(matrix == null || matrix.length == 0)
+            return;
+        
+        int n = matrix.length;
+        for(int i = 0; i < n / 2; i++){
+            for(int j = 0; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = temp;
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
